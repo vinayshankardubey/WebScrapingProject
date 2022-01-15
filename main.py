d21f80260d06a86a781f1a995bf7f2bbb9e0aa42
VinayDevs00 <vinaydevs00@gmail.com>
2022-01-15 16:40:35 +0530

    Login Completed

--- main.py
+++ main.py
@@ -17,14 +17,18 @@ def print_hi(name):
 
 
 
-def get_amazon_data():
+def facebook_login():
     options = Options()
     options.add_argument("user-data-dir=C:\\Users\\vinay\\AppData\\Local\\Google\\Chrome\\User Data")
     driver = webdriver.Chrome(executable_path=r'C:\\Users\\vinay\\Downloads\\chromedriver_win32\\chromedriver.exe', chrome_options=options)
-    driver.get("facebook.com")
+    driver.get("https://docs.google.com/spreadsheets/d/1e5XMpCKyzxUHaRY_Ug6QZC_-epogJb3QMZS09GHBeJQ/edit#gid=0")
+
+
+
+
     time.sleep(900000)
 
 
 if __name__ == '__main__':
-    get_amazon_data()
+    facebook_login()
 
